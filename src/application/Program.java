e63531422d6b3335cbbd71061de23b02ac7b5cf0
Mariana Tavares <marianasr.tavares@gmail.com>
2021-09-10 07:26:30 +0100

    First Class Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Position pos1= new Position(2,3);
+		System.out.println(pos1);
 	}
 
 }
