f255d548d8e58adbe5a2768ed79e8ce04401dced
Mariana Tavares <marianasr.tavares@gmail.com>
2021-09-12 18:36:09 +0100

    Little improvement in board printing

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -13,5 +13,17 @@ public class ChessPiece extends Piece {
 	
 	}
 
+
+	public Color getColor() {
+		return color;
+	}
+
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	
+	
+
 	
 }
