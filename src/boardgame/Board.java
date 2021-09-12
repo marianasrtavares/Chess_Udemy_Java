054f6f40bcbf9e885eefdf5657e25964f6e8a163
Mariana Tavares <marianasr.tavares@gmail.com>
2021-09-12 16:17:47 +0100

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
   
   
 public Board(int row, int column) {
+	if(row<1 || column<1) {
+		throw new BoardException("Error creating board. there must be at least 1 row and 1 column");
+	}
 	this.row = row;
 	this.column = column;
 	pieces= new Piece[row][column];
@@ -20,41 +23,49 @@ public int getRow() {
 	
 }
 
-
-
-public void setRow(int row) {
-	this.row = row;
-}
-
-
-
 public int getColumn() {
 	return column;
 }
 
-
-
-public void setColumn(int column) {
-	this.column = column;
-}
-
-
-
 public Piece piece (int row, int column) {
+	if(!positionExists(row, column)) {
+		throw new BoardException("That position doesnt exists o the board");
+	}
 	return pieces[row][column];
   
-
 }
 
 public Piece piece(Position position) {
+	if(!positionExists(position)) {
+		throw new BoardException("That position doesnt exists o the board");
+	}
+	
 	return pieces[position.getRow()][position.getColumn()];
 }
 
 public void placePiece( Piece piece, Position position) {
+	if(thereIsAPiece(position)) {
+		throw new BoardException("There is already a piece on position ");
+	}
 		pieces[position.getRow()][position.getColumn()]=piece;
 		piece.position=position;
 }
-		
+
+private boolean positionExists(int row, int column) {
+	return row<= this.row &&  row>=0 && column>=0 && column<=this.column; 
+}
+
+
+public boolean positionExists (Position position) {
+	return positionExists(position.getRow(), position.getColumn());
+}
+
+public boolean thereIsAPiece(Position position) {
+	if(!positionExists(position)) {
+		throw new BoardException("That position doesnt exists o the board");
+	}
+	return piece(position)!=null;
+}
 }
   
 
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -2,6 +2,7 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -23,8 +24,8 @@ public class ChessMatch {
 	
 	public void initialSetup() {
 		board.placePiece(new Rook (board,Color.WHITE), new Position(2,1));
-		board.placePiece(new Rook (board,Color.WHITE), new Position(2,1));
-		board.placePiece(new Rook (board,Color.WHITE), new Position(2,1));
+		board.placePiece(new King (board,Color.BLACK), new Position(0,4));
+		board.placePiece(new Rook (board,Color.WHITE), new Position(7,1));
 	}
 	
 	
